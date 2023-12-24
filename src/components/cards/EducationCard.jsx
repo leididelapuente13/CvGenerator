a3a6cb25e089b39dbf1d22797cf6a876d61a63f1
Leidi <leididelapuente3@gmail.com>
2023-12-23 23:11:58 -0500

    feat(component): add format to the dates
    "fix 10 in EducationForm", "re 10 in EducationCard"

--- src/components/cards/EducationCard.jsx
+++ src/components/cards/EducationCard.jsx
@@ -1,12 +1,13 @@
 import {PropTypes} from 'prop-types'
-  
+import moment from "moment";
+
 export const EducationCard = ({education}) => {
   return (
     <div>
         <p>{education.school}</p>
         <p>{education.degree}</p>
-        <p>{education.startDate}</p>
-        <p>{education.endDate}</p>
+        <p>{moment(education.startDate, "YYYY-MM-DD").format("DD-MMMM-YYYY")}</p>
+        <p>{education.endDate == "" ? "On going" : moment(education.endDate, "YYYY-MM-DD").format("DD-MMMM-YYYY")}</p>
         <p>{education.location}</p>
     </div>
   )
--- src/components/forms/EducationForm.jsx
+++ src/components/forms/EducationForm.jsx
@@ -8,7 +8,7 @@ const EducationForm = () => {
     school: "",
     degree: "",
     startDate: "",
-    endDate: "On going", 
+    endDate: "", 
     location: "",
   }); 
 
